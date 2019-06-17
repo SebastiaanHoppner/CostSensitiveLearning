22cf2998bbac3ae21f8d025cdf7c2d0d0a9db2d5
Sebastiaan Höppner <sebastiaan.hoppner@gmail.com>
2019-06-17 15:59:55 +0200

    Add files via upload

--- cslogit/R/computeObjectiveGradient.R
+++ cslogit/R/computeObjectiveGradient.R
@@ -1,4 +1,4 @@
-computeObjectiveGradient <- function (betas, X, tX, cost_matrix, diff_cost_matrix, lambda) {
+computeObjectiveGradient <- function (betas, X, tX, cost_matrix, diff_cost_matrix, nrowX, lambda) {
   # objective value = average expected cost + lambda * L1 norm of betas (without intercept)
   expterm <- as.numeric(exp(-betas %*% tX))
   if (any(is.infinite(expterm))) {
@@ -7,7 +7,7 @@ computeObjectiveGradient <- function (betas, X, tX, cost_matrix, diff_cost_matri
   scores1 <- 1 / (1 + expterm)
   scores0 <- 1 - scores1
 
-  return(list(objective = 2 * mean(cost_matrix * matrix(c(scores1, scores0), ncol = 2)) +
+  return(list(objective = sum(cost_matrix * matrix(c(scores1, scores0), ncol = 2)) / nrowX +
                 lambda * sum(abs(betas[-1])),
               gradient = (diff_cost_matrix * scores1 * scores0) %*% X +
                 lambda * c(0, sign(betas[-1])),
