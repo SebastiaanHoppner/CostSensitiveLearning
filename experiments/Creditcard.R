ab49801ec5c16bbdd3f9f6ab8b15520e8c431e32
Sebastiaan Höppner <sebastiaan.hoppner@gmail.com>
2020-03-26 12:21:49 +0100

    Add files via upload

--- experiments/Creditcard.R
+++ experiments/Creditcard.R
@@ -2,39 +2,8 @@
 # https://www.kaggle.com/mlg-ulb/creditcardfraud
 
 
-# - Instance-dependent cost-sensitive logistic regression with lasso regularization
-# - Instance-dependent cost-sensitive gradient tree boosting
-# - Regular logistic regression
-# - Extreme gradient boosted trees
-
-# TODO:
-#  - cs.cslogit for lambda
-#  - glmnet instead of glm
-#  - fixed_cost?
-#  - nrounds?
-#  - base_score?
-
-
 
 rm(list = ls())
-# Setup -------------------------------------------------------------------------------------------
-
-# save results as - - - - - - - - - - - - - - - -
-version <- "3-10"
-save_results_as <- paste0("Creditcard_", gsub("-", "", Sys.Date()), "_", version, ".RData")
-setwd("~/Desktop/PhD/PhD KUL/CostSensitiveLearning/experiments")
-
-
-# fixed cost - - - - - - - - - - - - - - - - - - -
-fixed_cost <- 10 # <=== !!! # sum(creditcard$Amount[creditcard$Class == 1]) / nrow(creditcard) = 0.2124798
-
-
-# procedure - - - - - - - - - - - - - - - - - - -
-nruns  <- 5
-nfolds <- 2
-
-
-
 # Load packages -----------------------------------------------------------------------------------
 library(csboost)
 library(cslogit)
@@ -47,7 +16,16 @@ library(splitstackshape)
 
 
 # Load and preprocess data ------------------------------------------------------------------------
-load("../data/creditcard.RData") # creditcard <- read.csv("../data/creditcard.csv")
+load("creditcard_part1.RData")
+load("creditcard_part2.RData")
+load("creditcard_part3.RData")
+
+creditcard <- rbind.data.frame(creditcard_part1,
+                               creditcard_part2,
+                               creditcard_part3)
+
+rm(creditcard_part1, creditcard_part2, creditcard_part3)
+
 
 creditcard <- creditcard[-which(creditcard$Amount == 0), ] # remove transactions with zero Amount
 creditcard <- creditcard[, -1] # remove variable Time
@@ -75,6 +53,7 @@ print(table(Class = creditcard$Class, amount_category = amount_category))
 
 
 # Create cost matrix ------------------------------------------------------------------------------
+fixed_cost <- 10
 cost_matrix <- matrix(nrow = nrow(creditcard), ncol = 2)
 cost_matrix[, 1] <- ifelse(creditcard$Class == 1, fixed_cost, 0)
 cost_matrix[, 2] <- ifelse(creditcard$Class == 1, amount, fixed_cost)
@@ -82,6 +61,8 @@ cost_matrix[, 2] <- ifelse(creditcard$Class == 1, amount, fixed_cost)
 
 
 # Create folds (nruns x nfolds) for cross-validation ----------------------------------------------
+nruns  <- 5
+nfolds <- 2
 set.seed(2019)
 cvfolds <- list()
 for (k in 1:nruns) {
@@ -218,49 +199,13 @@ for (k_run in 1:nruns) {
       predicted_classes <- ifelse(scores > threshold, 1, 0)
       metrics      <-     cslogit::performance(scores, predicted_classes, true_classes)$metrics
       cost_metrics <- cslogit::costPerformance(scores, predicted_classes, true_classes, cost_matrix)
-      spearman_correlation <- cor(scores[true_classes == 1], amount_test[true_classes == 1], method = "spearman")
-      return(cbind.data.frame(metrics, cost_metrics, cor = spearman_correlation))
+      return(cbind.data.frame(metrics, cost_metrics))
     }
     
     results_cslogit <- rbind.data.frame(results_cslogit, performanceMeasures(scores_cslogit_test, threshold_test, test$Class, cost_matrix_test))
     results_logit   <- rbind.data.frame(results_logit,   performanceMeasures(scores_logit_test,   threshold_test, test$Class, cost_matrix_test))
     results_csboost <- rbind.data.frame(results_csboost, performanceMeasures(scores_csboost_test, threshold_test, test$Class, cost_matrix_test))
     results_xgboost <- rbind.data.frame(results_xgboost, performanceMeasures(scores_xgboost_test, threshold_test, test$Class, cost_matrix_test))
-    
-    cat(paste("\n  - cslogit: savings =", round(results_cslogit$savings[length(results_cslogit$savings)], 4)))
-    cat(paste("\n  -   logit: savings =", round(  results_logit$savings[length(results_logit$savings)],   4)))
-    cat(paste("\n  - csboost: savings =", round(results_csboost$savings[length(results_csboost$savings)], 4)))
-    cat(paste("\n  - xgboost: savings =", round(results_xgboost$savings[length(results_xgboost$savings)], 4), "\n"))
-    
-    cat(paste("\n  - cslogit: cor =", round(results_cslogit$cor[length(results_cslogit$cor)], 4)))
-    cat(paste("\n  -   logit: cor =", round(  results_logit$cor[length(results_logit$cor)],   4)))
-    cat(paste("\n  - csboost: cor =", round(results_csboost$cor[length(results_csboost$cor)], 4)))
-    cat(paste("\n  - xgboost: cor =", round(results_xgboost$cor[length(results_xgboost$cor)], 4), "\n"))
-    
-    
-    # Lift curve - - - - - - - - - - - - - - - - -
-    scores_cslogit_test_fraud <- scores_cslogit_test[test$Class == 1]
-    scores_logit_test_fraud   <- scores_logit_test[test$Class == 1]
-    scores_csboost_test_fraud <- scores_csboost_test[test$Class == 1]
-    scores_xgboost_test_fraud <- scores_xgboost_test[test$Class == 1]
-    
-    amount_test_fraud <- amount_test[test$Class == 1]
-    
-    plot(scores_cslogit_test_fraud[order(scores_cslogit_test_fraud, decreasing = TRUE)],
-         cumsum(amount_test_fraud[order(scores_cslogit_test_fraud, decreasing = TRUE)] / sum(amount_test_fraud)),
-         type = "l", lwd = 2, las = 1, ylab = "% detected of all fraud amount", xlab = "fraud score",
-         main = "cslogit (solid), logit (dashed)", xlim = c(1, 0), cex.lab = 1.3)
-    lines(scores_logit_test_fraud[order(scores_logit_test_fraud, decreasing = TRUE)],
-          cumsum(amount_test_fraud[order(scores_logit_test_fraud, decreasing = TRUE)] / sum(amount_test_fraud)),
-          lwd = 2, las = 1, lty = 2)
-    
-    plot(scores_csboost_test_fraud[order(scores_csboost_test_fraud, decreasing = TRUE)],
-         cumsum(amount_test_fraud[order(scores_csboost_test_fraud, decreasing = TRUE)] / sum(amount_test_fraud)),
-         type = "l", lwd = 2, las = 1, ylab = "% detected of all fraud amount", xlab = "fraud score",
-         main = "csboost (solid), xgboost (dashed)", xlim = c(1, 0), cex.lab = 1.3)
-    lines(scores_xgboost_test_fraud[order(scores_xgboost_test_fraud, decreasing = TRUE)],
-          cumsum(amount_test_fraud[order(scores_xgboost_test_fraud, decreasing = TRUE)] / sum(amount_test_fraud)),
-          lwd = 2, las = 1, lty = 2)
     
     
     # Elapsed time - - - - - - - - - - - - - - - - -
@@ -298,9 +243,6 @@ times <- data.frame(time = c(time_cslogit, time_logit, time_csboost, time_xgboos
 average_times <- aggregate(times, by = list(methods_vec), FUN = mean)
 average_times <- average_times[order(match(average_times$Group.1, methods)), ]
 times$method <- methods_vec
-
-
-save.image(file = paste0("results Creditcard/", save_results_as))
 
 
 
@@ -364,48 +306,12 @@ box_F1 <- createBoxplots(ylabel = "F1 (%)",
                                     min(100 * max(results$F1, na.rm = TRUE) + 10, 100)),
                          average_measures = 100 * average_results$F1)
 
-box_AUCpr <- createBoxplots(ylabel = "AUC-PR (%)",
-                            df = data.frame(measure = 100 * results$AUC_pr,
-                                            method  = results$method),
-                            ylimit = c(100 * min(results$AUC_pr, na.rm = TRUE) - 10,
-                                       min(100 * max(results$AUC_pr, na.rm = TRUE) + 10, 100)),
-                            average_measures = 100 * average_results$AUC_pr)
-
-box_dfar <- createBoxplots(ylabel = "Detected fraud amount ratio (%)",
-                           df = data.frame(measure = 100 * results$detected_fraud_amount_ratio,
-                                           method  = results$method),
-                           ylimit = c(100 * min(results$detected_fraud_amount_ratio, na.rm = TRUE) - 10,
-                                      min(100 * max(results$detected_fraud_amount_ratio, na.rm = TRUE) + 10, 100)),
-                           average_measures = 100 * average_results$detected_fraud_amount_ratio)
-
-box_cor <- createBoxplots(ylabel = "Spearman correlation (%) - Amount vs Score (frauds)",
-                          df = data.frame(measure = 100 * results$cor,
-                                          method  = results$method),
-                          ylimit = c(100 * min(results$cor, na.rm = TRUE) - 10,
-                                     min(100 * max(results$cor, na.rm = TRUE) + 10, 100)),
-                          average_measures = 100 * average_results$cor)
-
-box_cost <- createBoxplots(ylabel = "Cost",
-                              df = data.frame(measure = results$average_cost,
-                                              method  = results$method),
-                              ylimit = c(min(results$average_cost, na.rm = TRUE),
-                                         min(max(results$average_cost, na.rm = TRUE))),
-                              average_measures = average_results$average_cost)
-
 
 
 # Boxplots - - - - - - - - - - - - - - - - - - - -
-# grid.arrange(box_expected_savings, box_savings, box_dfar,
-#              box_precision, box_recall, box_F1, box_AUCpr,
-#              layout_matrix = rbind(c(1, 5), c(2, 6), c(3, 7), c(4, 8)))
-#
-
 grid.arrange(box_expected_savings, box_savings,
              box_precision, box_recall, box_F1,
              layout_matrix = rbind(c(1, 3), c(1, 3), c(1, 4), c(2, 4), c(2, 5), c(2, 5)))
-
-# grid.arrange(box_expected_savings, box_savings,
-#              layout_matrix = rbind(c(1, 2)))
 
 
 
@@ -417,14 +323,4 @@ grid.arrange(box_expected_savings, box_savings,
 #                            average_measures = average_times$time)
 # plot(box_time)
 
-
-
-# Importance plots - - - - - - - - - - - - - - -
-# xgb.plot.importance(xgb.importance(model = csbtree$xgbmodel))
-# xgb.plot.importance(xgb.importance(model = xgbtree))
-
-
-print(cbind.data.frame(total_fraud_amount = results_cslogit$total_fraud_amount,
-                       total_fixed_cost = results_cslogit$total_fixed_cost,
-                       check = results_cslogit$total_fraud_amount < results_cslogit$total_fixed_cost))
 
